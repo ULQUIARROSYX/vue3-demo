854a45d7e39cca8e993b7c52bd8d53e0a6dcac3c
ULQUIARROSYX <1127781044@qq.com>
2022-12-01 01:39:18 +0800

    2

---
+++ mock.js
@@ -0,0 +1,55 @@
+const Koa = require('koa');
+const router = require('koa-router')();
+const koaBody = require('koa-body').koaBody();
+const app = new Koa();
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = {
+        a: 1,
+        b: '123'
+    }
+});
+
+router.get('/getUserList', async (ctx, next) => {
+    ctx.response.body = {
+        status: true,
+        data: [
+            { id: 'N001', name: '张珊', phonenumber: '13912341000', birth: '1998-01-08' },
+            { id: 'N002', name: '李珊', phonenumber: '13912342000', birth: '1998-01-08' },
+            { id: 'N003', name: '旺珊', phonenumber: '13912343000', birth: '1998-01-08' },
+        ],
+        msg: '获取数据成功'
+    }
+});
+
+router.post('/api/login', koaBody, async (ctx, next) => {
+    var
+        username = ctx.request.body.username || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with username: ${username}, password: ${password}`);
+    if (username === 'admin' && password === '123456') {
+        ctx.response.body = {
+            status: true,
+            data: null,
+            mag: '登录成功'
+        }
+    } else {
+        ctx.response.body = {
+            status: false,
+            data: null,
+            mag: '用户名或密码错误'
+        }
+    }
+})
+
+// add router middleware:
+app.use(router.routes());
+
+app.listen(3000);
+console.log('app started at port 3000...');
